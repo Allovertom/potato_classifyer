5ef16b9b6d06d27d29323daceadc5b947008a199
root <modanasianstyle20@gmail.com>
2020-03-22 14:42:46 +0900

    experiment mode and text box add

--- ModeChange.py
+++ ModeChange.py
@@ -1,33 +1,56 @@
 # mode change class
 import tkinter as tk
+from picamera import PiCamera
+from time import sleep
+import datetime
 
 class MChange:
     def __init__(self):
-        pass
-    def learning(self):
-        pass
+        self.camera = PiCamera()
+        self.camera.resolution = (64,64)
+    def text_print(self, tex):
+        self.txt.delete(0,tk.END)
+        self.txt.insert(tk.END,tex)
+        print(tex)
+    def take_pics(self):
+        dt_now = datetime.datetime.now().strftime('%Y_%m_%d_%H:%M:%S')
+        self.camera.start_preview(fullscreen=False, window=(0, 0, 500, 500))
+        sleep(1.7)
+        try:
+            self.camera.capture('//home/pi/ドキュメント/potato_classfier/predict/%s.jpg' % dt_now)
+        finally:
+            self.camera.stop_preview()
+        self.text_print("しゃしんはほぞんされました。")
     def A_take_pics_and_move(self):
         pass
     def B_take_pics_and_move(self):
         pass
-    
+    def learning(self):
+        pass
     def select_mode(self):
         # ウィンドウを生成してそのウィンドウを操作するための値をrootに代入します。
         root = tk.Tk()
         # ウィンドウ名を指定します
         root.title('選択')
         # ウィンドウサイズを指定します。('x'は、小文字のエックスです。)
-        root.geometry('225x70')
+        root.geometry('400x400')
+        
+        #テキストボックスの定義
+        self.txt = tk.Entry(width=50)
+        self.txt.place(x=5, y=300)
 
 
         #ボタンの定義
-        ButtonA = tk.Button(root,text="A品さつえい", command=self.A_take_pics_and_move, width=12, height=3)
-        ButtonB = tk.Button(root,text="B品さつえい", command=self.B_take_pics_and_move, width=12, height=3)
-        ButtonC = tk.Button(root,text="がくしゅう", command=self.learning, width=12, height=3)
+        btnA = tk.Button(root,text="ためしどり", command=self.take_pics, width=12, height=3)
+        btnB = tk.Button(root,text="A品さつえい", command=self.A_take_pics_and_move, width=12, height=3)
+        btnC = tk.Button(root,text="B品さつえい", command=self.B_take_pics_and_move, width=12, height=3)
+        btnD = tk.Button(root,text="がくしゅう", command=self.learning, width=12, height=3)
         
         #ボタンの並びを定義
-        ButtonA.grid(row=0, column=0)
-        ButtonB.grid(row=0, column=1)
+        btnA.grid(row=0, column=1)
+        btnB.grid(row=1, column=1)
+        btnC.grid(row=2, column=1)
+        btnD.grid(row=3, column=1)
 
         # ウィンドウを表示して制御するためのループに入ります。
         root.mainloop()
