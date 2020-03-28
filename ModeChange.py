4399c62effa46923f161f5a5878fe5fa648c217b
root <modanasianstyle20@gmail.com>
2020-03-28 15:00:57 +0900

    NoObject pic btn add

--- ModeChange.py
+++ ModeChange.py
@@ -42,9 +42,10 @@ class MChange:
         self.take_pics(1)
     def take_pics_B(self):
         self.take_pics(2)
-    
+    def take_pict_No(self):
+        self.take_pics(3)
     def learning(self):
-        pass
+        print("がくしゅう中")
     def select_mode(self):
         # ウィンドウを生成してそのウィンドウを操作するための値をrootに代入します。
         root = tk.Tk()
@@ -55,7 +56,7 @@ class MChange:
         
         #テキストボックスの定義
         self.txt = tk.Entry(width=50)
-        self.txt.place(x=5, y=300)
+        self.txt.place(x=5, y=350)
         
         #保存先フォルダ
         #self.Path = '/home/pi/ドキュメント/potato_classfier/train'
@@ -64,13 +65,15 @@ class MChange:
         btnA = tk.Button(root,text="ためしどり", command=self.take_pics, width=12, height=3)
         btnB = tk.Button(root,text="A品さつえい", command=self.take_pics_A, width=12, height=3)
         btnC = tk.Button(root,text="B品さつえい", command=self.take_pics_B, width=12, height=3)
-        btnD = tk.Button(root,text="がくしゅう", command=self.learning, width=12, height=3)
+        btnD = tk.Button(root,text="何もないさつえい", command=self.take_pict_No, width=12, height=3)
+        btnE = tk.Button(root,text="がくしゅう", command=self.learning, width=12, height=3)
         
         #ボタンの並びを定義
         btnA.grid(row=0, column=1)
         btnB.grid(row=1, column=1)
         btnC.grid(row=2, column=1)
         btnD.grid(row=3, column=1)
+        btnE.grid(row=4, column=1)
 
         # ウィンドウを表示して制御するためのループに入ります。
         root.mainloop()
